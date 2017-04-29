37b0f9e74e6eede099ffd6643a26c177efcc767a
Fatih BAKIR <mfatihbakir@gmail.com>
2017-04-29 13:31:14 +0300

    tool to find required messages from a module

--- malt_game/main.cpp
+++ malt_game/main.cpp
@@ -7,10 +7,11 @@
 #include <sample/components/simple.hpp>
 #include <malt_basic/components/transform.hpp>
 
-
 int main()
 {
     malt::entity e = malt::create_entity();
+
+    malt::entity another = malt::create_entity();
 
     e.add_component<simple>();
 
@@ -20,6 +21,8 @@ int main()
 
     e.get_component<transform>()->x = 3;
     e.get_component<transform>()->translate(1, 2, 3);
+
+    another.add_component<test_component>()->data = 10;
 
     malt::broadcast(int{});
 
--- sample/components/test.cpp
+++ sample/components/test.cpp
@@ -9,6 +9,7 @@
 void test_component::Handle(int)
 {
     get_entity().deliver_message(render{}, 16.6f);
+    get_entity().deliver_message(render{}, 16.6f, 3.2);
     get_entity().deliver_message(int{}, 8);
     std::cout << "hello world" << data << "\n";
 }
