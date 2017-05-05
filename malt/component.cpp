cf8bc211e8324e690eec35477427eed62f81e4a2
Fatih BAKIR <mfatihbakir@gmail.com>
2017-05-06 01:56:13 +0300

    moved core to own repo

--- malt/component.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by fatih on 4/27/17.
-//
-
-#include <malt/component.hpp>
-
-malt::entity malt::component::get_entity() const
-{ return m_e; }
--- malt/malt_core-config.cmake
+++
@@ -1,4 +0,0 @@
-cmake_minimum_required(VERSION 3.5)
-
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/malt_core.cmake)
