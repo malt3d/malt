5ed14d3caedf73d9a3ba9b8eb3f336c8927384b7
Fatih BAKIR <mfatihbakir@gmail.com>
2017-05-05 23:21:17 +0300

    tool submodule

--- malt_tooling/cmake/MaltTools.cmake
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 3.5)
-project(malt_tooling)
-
-function(get_sources name)
-    get_target_property(test malt_render SOURCES)
-    return(test)
-endfunction()
--- malt_tooling/list_components.py
+++
@@ -1,3 +0,0 @@
-# find all components defined by a module
-# warn if a class inherited from malt::component is not MALT_IMPLEMENT_COMP'ed
-
